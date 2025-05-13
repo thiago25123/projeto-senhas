c6f037cd68c38011a0d4eb51738be63d3a7eb866
thiago25123 <147066617+thiago25123@users.noreply.github.com>
2025-05-13 09:22:07 -0300

    gbfgbfgn

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
  const numeroSenha=document.querySelector(".parametro-senha__texto");
+let tamanhoSenha=12
+ numeroSenha.textContent=tamanhoSenha;
 
- numeroSenha.textContent=5;
+ const botoes= document.querySelectorAll(".parametro-senha__botao");
+ botoes[0].onclick = diminuiTamanho;
+
+ function diminuiTamanho(){
+tamanhoSenha=tamanhoSenha-1;
+numeroSenha.textContent=tamanhoSenha;
+
+ }
